70f47fc191f9ee40fd25cf418d4e3da34dd8306a
Ve1nsStp <jmcgtheshy@gmail.com>
2024-05-29 11:28:12 +0800

    unit4for循环

---
+++ goproject/src/gocode/testproject01/unit4/demo05/main.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+    // 实现一个功能，求和
+    var i1 int = 1
+    var i2 int = 2
+    var i3 int = 3
+    var i4 int = 4
+    var i5 int = 5
+    // 定义一个变量接收这个和
+    var sum int = 0
+}
