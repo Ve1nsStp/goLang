7aba429cc2b714cadc71df80d4cc404c5ba5ae82
Ve1nsStp <jmcgtheshy@gmail.com>
2024-05-30 18:10:00 +0800

    unit4for循环2

--- goproject/src/gocode/testproject01/unit4/demo05/main.go
+++ goproject/src/gocode/testproject01/unit4/demo05/main.go
@@ -2,11 +2,18 @@ package main
 import "fmt"
 func main(){
     // 实现一个功能，求和
-    var i1 int = 1
-    var i2 int = 2
-    var i3 int = 3
-    var i4 int = 4
-    var i5 int = 5
+//     var i1 int = 1
+//     var i2 int = 2
+//     var i3 int = 3
+//     var i4 int = 4
+//     var i5 int = 5
+//        var i int = 1
     // 定义一个变量接收这个和
     var sum int = 0
+    for var i int = 1 ; i < 6 ; i++ {
+    sum += i
+    }
+
+    // 输出结果
+    fmt.Println(sum)
 }
